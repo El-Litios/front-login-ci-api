476887870802250d6ce6ecf38482f18b5f3bc196
El-Litios <carlosguzmanrojas00@gmail.com>
2022-04-09 16:35:27 -0400

    Formulario para login

---
+++ src/components/FormLogin.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <input placeholder="email" type="text" v-model="user.email">
+    <input placeholder="password" type="text" v-model="user.password">
+    <button @click="doLogin()" >Enviar</button>
+  </div>
+</template>
+
+<script>
+import {mapActions} from 'vuex'
+
+export default {
+  name: 'FormLogin',
+  data () {
+    return {
+      user: {}
+    }
+  },
+
+  methods: {
+    ...mapActions(['login']),
+
+    doLogin(){
+      this.login(this.user)
+    }
+  }
+}
+</script>
