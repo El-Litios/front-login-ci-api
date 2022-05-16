bfc0a38c27177977930f4d103387d48178714c85
El-Litios <carlosguzmanrojas00@gmail.com>
2022-05-15 23:05:07 -0400

    Vista para gestion de clientes

---
+++ src/views/Client.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Navbar />
+    <table-client :clients="client.client"/>
+  </div>
+</template>
+
+<script>
+import TableClient from "../components/ClientTable.vue";
+import Navbar from "../components/Navbar.vue";
+import { mapActions, mapState } from "vuex";
+export default {
+  name: "Client",
+  components: {
+    TableClient,
+    Navbar,
+  },
+
+  data() {
+    return {};
+  },
+
+  computed: {
+    ...mapState(["client"]),
+  },
+
+  methods: {
+    ...mapActions(["getClients"]),
+  },
+
+  created() {
+    this.getClients();
+  },
+};
+</script>
+
+<style>
+</style>
