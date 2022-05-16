8871c694a33e860d6cd1a973945c3404f491901a
El-Litios <carlosguzmanrojas00@gmail.com>
2022-05-15 23:05:19 -0400

    Pagina principal

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Navbar/>
+    <h1>Inicio</h1>
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+export default {
+  name: 'Dashboard',
+  components: {
+    Navbar
+  },
+  
+}
+</script>
