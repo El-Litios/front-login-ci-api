b723b4a7f16e51414e3c989533c119b19351e547
El-Litios <carlosguzmanrojas00@gmail.com>
2022-05-15 23:03:12 -0400

    Quita barra de navegacion de componente principal

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav" >
-      <div v-if="initialState.user && initialState.user !== 'null'">
-        <router-link  v-if="initialState.user.user.roleid == 1" to="/">Home</router-link> |
-        <router-link  v-if="initialState.user.user.roleid == 2" to="/about">About</router-link> |
-      </div>
-      <button v-if="initialState.user !== 'null' && initialState.user" @click="logout()">Logout</button>
-    </div>
     <router-view/>
   </div>
 </template>
@@ -14,13 +7,7 @@
 <script>
 import { mapActions, mapState } from 'vuex'
 export default {
-  methods: {
-    ...mapActions(['logout']),
-  },
-
-  computed: {
-    ...mapState(['initialState'])
-  }
+  name: 'App',
 }
 </script>
 
@@ -31,18 +18,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
