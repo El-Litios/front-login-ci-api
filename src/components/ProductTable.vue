c232806ea297b5d383e515ce6c7cb97c82d3bb12
El-Litios <carlosguzmanrojas00@gmail.com>
2022-05-29 21:23:17 -0400

    Tabla de listado de productos

---
+++ src/components/ProductTable.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Lista de productos</h1>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-center">Nombre</th>
+            <th class="text-center">Descripcion</th>
+            <th class="text-center">Stock</th>
+            <th class="text-center">Creado en:</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in products" :key="item.id">
+            <td>{{ item.name }}</td>
+            <td>{{ item.description }}</td>
+            <td>{{ item.stock }}</td>
+            <td>{{ item.created_at }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        products: {
+            Type: Array,
+            required: true
+        }
+    },
+
+    created(){
+      console.log();
+    }
+}
+</script>
+
+<style>
+
+</style>
