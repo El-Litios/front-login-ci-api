ca1b1a8c098d3d2712cab7c66f2b76c3eb57ca4f
El-Litios <carlosguzmanrojas00@gmail.com>
2022-05-29 21:24:54 -0400

    agrega mutacion de eliminacion de cliente dentro del arreglo

--- src/store/client/mutations.js
+++ src/store/client/mutations.js
@@ -1,3 +1,7 @@
 export const setClient = (state, payload) => {
-    state.client = payload;
-  };
+  state.client = payload;
+};
+
+export const deletedClient = (state, payload) => {
+  state.client = Object.values(state.client).filter((c) => c.id !== payload);
+};
