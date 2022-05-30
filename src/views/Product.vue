131eb0162ccdb0912ffe1b5f7d77db452ca3f5f7
El-Litios <carlosguzmanrojas00@gmail.com>
2022-05-29 21:23:30 -0400

    Vista de productos

---
+++ src/views/Product.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Navbar />
+    <ProductTable :products="product.product"/>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from "vuex";
+import ProductTable from "../components/ProductTable.vue";
+import Navbar from "../components/Navbar.vue";
+export default {
+  components: {
+    Navbar,
+    ProductTable,
+  },
+  computed: {
+    ...mapState("product", ["product"]),
+  },
+
+  methods: {
+    ...mapActions("product", ["getProduct"]),
+  },
+
+  created() {
+    this.getProduct();
+  },
+};
+</script>
+
+<style>
+</style>
