ebdff464ec03fc4d827217962da638919dc94b2d
El-Litios <carlosguzmanrojas00@gmail.com>
2022-05-29 21:24:30 -0400

    funciones para CRUD

--- src/store/client/actions.js
+++ src/store/client/actions.js
@@ -1,40 +1,66 @@
 import api from "../../api/api";
 import AuthHeader from "../../services/auth-header";
-import router from '../../router'
+import router from "../../router";
 import authHeader from "../../services/auth-header";
 
 export const getClients = async ({ commit }) => {
-    try {
-        await api.get("/client", { headers: AuthHeader() }).then((res) => {
-          const clientData = {
-            client: res.data.client,
-            message: res.data.message,
-          };
-          console.log(clientData);
-          commit("setClient", clientData);
-        });
-      } catch (error) {
-        console.log(error);
-      }
+  try {
+    await api.get("/client", { headers: AuthHeader() }).then((res) => {
+      const clientData = {
+        client: res.data.client,
+        message: res.data.message,
+      };
+      commit("setClient", clientData);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const createClient = async ({}, client) => {
-  console.log('DATA:  ', client);
   try {
-    await api.post("/newclient", 
-    {name: client.name,
-      email: client.email,
-      retainer_fee: client.rent}
-    ,
-    authHeader())
+    await api
+      .post(
+        "/newclient",
+        { name: client.name, email: client.email, retainer_fee: client.rent },
+        authHeader()
+      )
+      .then((res) => {
+        console.log(res);
+        router.go();
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateClient = async ({}, client) => {
+  try {
+    await api.post("/updateclient",
+    { id: client.id, name: client.name, email: client.email, retainer_fee: client.rent },
+    authHeader()
+    )
+    .then(() => {
+      router.go();
+    })
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteClient = async ({ commit }, id) => {
+  try {
+    await api.delete(`/deleteclient/${id}`,
+    authHeader()
+    )
     .then(res => {
       console.log(res);
-      router.go()
-    })
-    .catch(error => {
-      console.log(error.response.data);  
-         console.log(error.response.status);  
-         console.log(error.response.headers);
+      commit('deletedClient', id)
     })
   } catch (error) {
     console.log(error);
