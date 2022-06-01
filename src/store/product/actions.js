aef486112899ab84452a3af871b93d4e5a1fc7ec
El-Litios <carlosguzmanrojas00@gmail.com>
2022-06-01 01:33:25 -0400

    Agrega accion destinada a guardar productos

--- src/store/product/actions.js
+++ src/store/product/actions.js
@@ -18,9 +18,24 @@ export const getProduct = async ({ commit }) => {
 export const getProductCategory = async ({ commit }) => {
   try {
     await api.get("/category", { headers: AuthHeader() }).then((res) => {
-      commit("setCategory", res.data.categories)
+      commit("setCategory", res.data.categories);
     });
   } catch (error) {
     console.log(error);
+  }
+};
+
+export const saveProducts = async ({}, product) => {
+  try {
+    await api.post("/saveproducts",
+    { name: product.name, description: product.description, categoryid: product.category, quantity: product.quantity , stateid: 2},
+    AuthHeader())
+    .then((res) => {
+      console.log(res.data.message);
+    });
+  } catch (error) {
+    console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
   }
 };
