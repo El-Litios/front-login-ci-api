d15e3989b51c5a1ac441c6d076033639e5213c6b
El-Litios <carlosguzmanrojas00@gmail.com>
2022-07-26 01:16:10 -0400

    Componente items del carro de compras

---
+++ src/components/ItemsCarro.vue
@@ -0,0 +1,25 @@
+<template>
+  <tr>
+    <td>{{item.name}}</td>
+    <td>{{item.quantity}}</td>
+    <td>
+        <v-btn>
+            +
+        </v-btn>
+        <v-btn>
+            -
+        </v-btn>
+    </td>
+    <td>{{item.precio * item.stock}}</td>
+  </tr>
+</template>
+
+<script>
+export default {
+props: ['item']
+}
+</script>
+
+<style>
+
+</style>
