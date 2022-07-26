0bf7749cadbf6c1eb7b1dcdeaa2d74b57201fb78
El-Litios <carlosguzmanrojas00@gmail.com>
2022-07-26 01:19:36 -0400

    Vista para el carro de compras, con lista

---
+++ src/views/ShoppingCart.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Carro de compras</h1>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-center">Nombre</th>
+            <th class="text-center">Cantidad</th>
+            <th class="text-center">Accion</th>
+            <th class="text-center">Subtotal</th>
+          </tr>
+        </thead>
+        <!-- Productos del carro -->
+        <tbody id="items">
+            <ItemsCarro v-for="item in cart" :key="item.id" :item="item"/>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </div>
+</template>
+
+<script>
+import ItemsCarro from '@/components/ItemsCarro'
+import { mapState } from 'vuex';
+
+export default {
+  components: {
+    ItemsCarro
+  },
+  computed: {
+    ...mapState('shoppingCart', ['cart'])
+  }
+};
+</script>
+
+<style>
+</style>
