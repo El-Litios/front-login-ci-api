8b42cdaa639db66420f9122b7ab46ae1732915f1
El-Litios <carlosguzmanrojas00@gmail.com>
2022-07-28 00:25:35 -0400

    Mutations para botones  del carro

--- src/store/shopping-cart/mutations.js
+++ src/store/shopping-cart/mutations.js
@@ -3,3 +3,17 @@ export const setCart = (state, payload) => {
   state.cart[payload.id] = payload
   console.log(state.cart);
 }
+
+export const addItemCount = (state, payload) => {
+  
+  state.cart[payload].quantity = state.cart[payload].quantity + 1
+  console.log('items agregados', state.cart[payload].quantity);
+}
+
+export const removeItemCount = (state, payload) => {
+  state.cart[payload].quantity = state.cart[payload].quantity - 1
+  console.log('items eliminados del carro', state.cart[payload].quantity);
+  if (state.cart[payload].quantity === 0) {
+    delete state.cart[payload]
+  }
+} 
