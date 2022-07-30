2bc95970a680b37476d6b68f667cc953c8ef355a
El-Litios <carlosguzmanrojas00@gmail.com>
2022-07-30 16:28:15 -0400

    Codigo que evita agregar mas cantidad al carro dependiendo del stock

--- src/store/shopping-cart/actions.js
+++ src/store/shopping-cart/actions.js
@@ -1,6 +1,12 @@
 export const addToCart = ({ commit, state }, product) => {
   state.cart.hasOwnProperty(product.id)
-    ? (product.quantity = state.cart[product.id].quantity + 1)
+    ? 
+      state.cart[product.id].stock > product.quantity
+        ? (product.quantity = state.cart[product.id].quantity + 1)
+        : alert('no puede')
+      
     : (product.quantity = 1);
+
+  
   commit("setCart", product);
 };
