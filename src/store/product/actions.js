c6dcfbc94d637f8e57cda71f791523be84c68adb
El-Litios <carlosguzmanrojas00@gmail.com>
2022-08-02 00:25:42 -0400

    Se envia el arreglo para la mutation

--- src/store/product/actions.js
+++ src/store/product/actions.js
@@ -4,11 +4,7 @@ import AuthHeader from "../../services/auth-header";
 export const getProduct = async ({ commit }) => {
   try {
     await api.get("/product", { headers: AuthHeader() }).then((res) => {
-      const productData = {
-        product: res.data.products,
-        message: res.data.message,
-      };
-      commit("setProduct", productData);
+      commit("setProduct", res.data.products);
     });
   } catch (error) {
     console.log(error);
