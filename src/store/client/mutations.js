a6a3bde57eea5734ec9376d1bbfc077162179d32
El-Litios <carlosguzmanrojas00@gmail.com>
2022-08-02 00:25:18 -0400

    Filtrado por arreglo en la eliminacion de clientes

--- src/store/client/mutations.js
+++ src/store/client/mutations.js
@@ -3,5 +3,5 @@ export const setClient = (state, payload) => {
 };
 
 export const deletedClient = (state, payload) => {
-  state.client = Object.values(state.client).filter((c) => c.id !== payload);
+  state.client = state.client.filter(c => c.id !== payload);
 };
