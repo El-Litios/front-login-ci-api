fd072f70cc4a7b41b328495f38b8d889c97371cf
El-Litios <carlosguzmanrojas00@gmail.com>
2022-08-15 20:26:34 -0400

    Selects con data de clientes y metodos de pago

--- src/components/ModalShoppingCart.vue
+++ src/components/ModalShoppingCart.vue
@@ -38,11 +38,37 @@
                     <v-btn @click="addItemCount(item.id)"> + </v-btn>
                     <v-btn @click="removeItemCount(item.id)"> - </v-btn>
                   </td>
-                  <td>{{ item.precio * item.stock }}</td>
+                  <td>{{ item.price * item.quantity }}</td>
                 </tr>
               </tbody>
+              <tfoot>
+                <tr>
+                  <th class="text-right" colspan="3"><h3>Total</h3></th>
+                  <th class="text-center">
+                    <h3>${{ getTotalPrice }}</h3>
+                  </th>
+                </tr>
+              </tfoot>
             </template>
           </v-simple-table>
+          <v-row justify="end">
+            <v-col md="4">
+              <v-select
+                :items="client"
+                item-text="name"
+                item-value="id"
+                label="Cliente"
+              ></v-select>
+            </v-col>
+            <v-col md="4">
+              <v-select
+                :items="paymentmethods"
+                item-text="description"
+                item-value="id"
+                label="Metodo de Pago"
+              ></v-select>
+            </v-col>
+          </v-row>
         </v-container>
       </v-card>
     </v-dialog>
@@ -50,10 +76,9 @@
 </template>
 
 <script>
-import { mapGetters, mapState, mapMutations } from "vuex";
+import { mapGetters, mapState, mapMutations, mapActions } from "vuex";
 
 export default {
-
   data() {
     return {
       dialog: false,
@@ -61,12 +86,18 @@ export default {
   },
 
   computed: {
-    ...mapState("shoppingCart", ["cart"]),
-    ...mapGetters("shoppingCart", ["getAllItemsCart"]),
+    ...mapState("shoppingCart", ["cart", "client", "paymentmethods"]),
+    ...mapGetters("shoppingCart", ["getAllItemsCart", "getTotalPrice"]),
   },
 
   methods: {
+    ...mapActions("shoppingCart", ["getClientInfo", "getPaymentMethods"]),
     ...mapMutations("shoppingCart", ["addItemCount", "removeItemCount"]),
+  },
+
+  created() {
+    this.getClientInfo();
+    this.getPaymentMethods();
   },
 };
 </script>
