ab90d5c786b47cda22dc756c5e992c1c931c310e
El-Litios <carlosguzmanrojas00@gmail.com>
2022-08-15 20:29:45 -0400

    Obtener el total del carro de compras

--- src/store/shopping-cart/getters.js
+++ src/store/shopping-cart/getters.js
@@ -1,3 +1,7 @@
 export const getAllItemsCart = (state) => {
     return state.cart
 }
+
+export const getTotalPrice = (state) => {
+    return Object.values(state.cart).reduce((prev, {quantity, price}) => prev + quantity * price, 0) 
+}
